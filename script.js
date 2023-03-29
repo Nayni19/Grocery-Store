33ad198fc8009d1d4eb29a9b11cde5ae4818f071
Nayni Shah <83155646+Nayni19@users.noreply.github.com>
2023-03-29 15:55:37 +0530

    Updated time and date

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Sept 31, 2022 00:00:00").getTime();
+var countDownDate = new Date("Sept 31, 2024 00:00:00").getTime();
 var x = setInterval(function () {
   var now = new Date().getTime();
   var distance = countDownDate - now;
